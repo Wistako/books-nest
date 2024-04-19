d7a6c980ff46452eaddcecbd1a5e9cad8adfbb5d
Piotr Słomczyński <wistako@gmail.com>
2024-04-19 14:58:15 +0200

    Delete users for admin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +24,13 @@ export class UsersController {
   @Get('/:id')
   getById(@Param('id', new ParseUUIDPipe()) id: User['id']) {
     return this.userService.getById(id);
+  }
+
+  @UseGuards(JwtAuthGuard, AdminAuthGuard)
+  @Delete('/:id')
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: User['id']) {
+    if (!(await this.userService.getById(id)))
+      throw new NotFoundException('User not found');
+    return this.userService.deleteById(id);
   }
 }
